731a3b757aca865c2c8acebc368688d6d1995e67
Chris Troutner <chris.troutner@gmail.com>
2018-10-28 20:17:18 -0700

    Created prototype for optimal UTXO selection

--- src/commands/send.js
+++ src/commands/send.js
@@ -33,7 +33,8 @@ class Send extends Command {
       // Ensure flags meet qualifiying critieria.
       this.validateFlags(flags)
 
-      const name = flags.name
+      const name = flags.name // Name of the wallet.
+      const bch = flags.bch // Amount to send in BCH.
 
       // Open the wallet data file.
       const walletInfo = appUtil.openWallet(name)
@@ -51,6 +52,8 @@ class Send extends Command {
       console.log(`utxos: ${util.inspect(utxos)}`)
 
       // Select optimal UTXO
+      const utxo = await this.selectUTXO(bch, utxos, BITBOX)
+      console.log(`selected utxo: ${util.inspect(utxo)}`)
 
       // Generate a new address
 
@@ -59,6 +62,34 @@ class Send extends Command {
       if (err.message) console.log(err.message)
       else console.log(`Error in .run: `, err)
     }
+  }
+
+  // Selects a UTXO from an array of UTXOs based on this optimization criteria:
+  // 1. The UTXO must be larger than or equal to the amount of BCH to send.
+  // 2. The UTXO should be as close to the amount of BCH as possible.
+  //    i.e. as small as possible
+  // Returns a single UTXO object.
+  selectUTXO(bch, utxos, BITBOX) {
+    let candidateUTXO = {}
+
+    // Loop through all the UTXOs.
+    for (var i = 0; i < utxos.length; i++) {
+      const thisUTXO = utxos[i]
+      // The UTXO must be greater than or equal to the send amount.
+      if (thisUTXO.amount >= bch) {
+        // Automatically assign if the candidateUTXO is an empty object.
+        if (!candidateUTXO.amount) {
+          candidateUTXO = thisUTXO
+          continue
+
+          // Replace the candidate if the current UTXO is closer to the send amount.
+        } else if (candidateUTXO.amount > thisUTXO.amount) {
+          candidateUTXO = thisUTXO
+        }
+      }
+    }
+
+    return candidateUTXO
   }
 
   // Returns an array of UTXO objects. These objects contain the metadata needed
@@ -99,6 +130,10 @@ class Send extends Command {
     if (!name || name === "")
       throw new Error(`You must specify a wallet with the -n flag.`)
 
+    const bch = flags.bch
+    if (isNaN(Number(bch)))
+      throw new Error(`You must specify a quantity in BCH with the -b flag.`)
+
     return true
   }
 }
@@ -106,7 +141,8 @@ class Send extends Command {
 Send.description = `Poll the network and update the balances of the wallet.`
 
 Send.flags = {
-  name: flags.string({ char: "n", description: "Name of wallet" })
+  name: flags.string({ char: "n", description: "Name of wallet" }),
+  bch: flags.string({ char: "b", description: "Quantity in BCH" })
 }
 
 module.exports = Send
