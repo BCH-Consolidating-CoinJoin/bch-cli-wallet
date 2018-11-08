eadbecc4dc654092705424657e2e8b8d38630d42
Chris Troutner <chris.troutner@gmail.com>
2018-11-08 13:18:49 -0800

    Successfully querying server stdout

--- src/commands/coinjoin.js
+++ src/commands/coinjoin.js
@@ -24,6 +24,7 @@ const BB = require("bitbox-sdk/lib/bitbox-sdk").default
 const appUtil = require("../util")
 //const GetAddress = require("./get-address")
 const UpdateBalances = require("./update-balances")
+const rp = require("request-promise")
 
 // Used for debugging and error reporting.
 const util = require("util")
@@ -54,7 +55,8 @@ class CoinJoin extends Command {
       else var BITBOX = new BB({ restURL: "https://rest.bitcoin.com/v1/" })
 
       // Query the server's standard BCH output.
-      const stdout = await getStdOut(flags.server)
+      const stdout = await this.getStdOut(server)
+      console.log(`stdout: ${stdout}`)
       if (!stdout) throw new Error(`Could not connect with CoinJoin server.`)
 
       // Update balances before sending.
@@ -63,12 +65,12 @@ class CoinJoin extends Command {
 
       // Get all UTXOs controlled by this wallet.
       const utxos = await appUtil.getUTXOs(walletInfo, BITBOX)
-      //console.log(`utxos: ${util.inspect(utxos)}`)
+      console.log(`utxos: ${util.inspect(utxos)}`)
 
       // Send the BCH, transfer change to the new address
-      const txid = await this.sendAllBCH(utxos, sendToAddr, walletInfo, BITBOX)
+      //const txid = await this.sendAllBCH(utxos, sendToAddr, walletInfo, BITBOX)
 
-      console.log(`TXID: ${txid}`)
+      //console.log(`TXID: ${txid}`)
     } catch (err) {
       //if (err.message) console.log(err.message)
       //else console.log(`Error in .run: `, err)
@@ -78,95 +80,25 @@ class CoinJoin extends Command {
 
   // Queries the server to get the standard BCH output value used by the server.
   // Returns a Number representing the amount of BCH.
-  // Returns false or throws error if there are issues.
+  // Throws error if there are issues.
   async getStdOut(server) {
     try {
-      return false
+      const options = {
+        method: "GET",
+        uri: `${server}/stdout`,
+        resolveWithFullResponse: true,
+        json: true,
+        headers: {
+          Accept: "application/json"
+        }
+      }
+
+      const result = await rp(options)
+
+      const stdout = result.body.stdout
+      return Number(stdout)
     } catch (err) {
       console.log(`Error in coinjoin.js/getStdOut()`)
-      throw err
-    }
-  }
-
-  // Sends BCH to
-  async sendAllBCH(utxos, sendToAddr, walletInfo, BITBOX) {
-    try {
-      //console.log(`utxos: ${util.inspect(utxos)}`)
-
-      // instance of transaction builder
-      if (walletInfo.network === `testnet`)
-        var transactionBuilder = new BITBOX.TransactionBuilder("testnet")
-      else var transactionBuilder = new BITBOX.TransactionBuilder()
-
-      let originalAmount = 0
-
-      // Calulate the original amount in the wallet and add all UTXOs to the
-      // transaction builder.
-      for (var i = 0; i < utxos.length; i++) {
-        const utxo = utxos[i]
-
-        originalAmount = originalAmount + utxo.satoshis
-
-        transactionBuilder.addInput(utxo.txid, utxo.vout)
-      }
-
-      // original amount of satoshis in vin
-      //console.log(`originalAmount: ${originalAmount}`)
-
-      // get byte count to calculate fee. paying 1 sat/byte
-      const byteCount = BITBOX.BitcoinCash.getByteCount(
-        { P2PKH: utxos.length },
-        { P2PKH: 1 }
-      )
-      //console.log(`fee: ${byteCount}`)
-
-      // amount to send to receiver. It's the original amount - 1 sat/byte for tx size
-      const sendAmount = originalAmount - byteCount
-      //console.log(`sendAmount: ${sendAmount}`)
-
-      // add output w/ address and amount to send
-      transactionBuilder.addOutput(
-        BITBOX.Address.toLegacyAddress(sendToAddr),
-        sendAmount
-      )
-
-      let redeemScript
-
-      // Loop through each input and sign
-      for (var i = 0; i < utxos.length; i++) {
-        const utxo = utxos[i]
-
-        // Generate a keypair for the current address.
-        const change = appUtil.changeAddrFromMnemonic(
-          walletInfo,
-          utxo.hdIndex,
-          BITBOX
-        )
-        const keyPair = BITBOX.HDNode.toKeyPair(change)
-
-        transactionBuilder.sign(
-          i,
-          keyPair,
-          redeemScript,
-          transactionBuilder.hashTypes.SIGHASH_ALL,
-          utxo.satoshis
-        )
-      }
-
-      // build tx
-      const tx = transactionBuilder.build()
-
-      // output rawhex
-      const hex = tx.toHex()
-      //console.log(`Transaction raw hex: ${hex}`)
-
-      // sendRawTransaction to running BCH node
-      const broadcast = await BITBOX.RawTransactions.sendRawTransaction(hex)
-      //console.log(`Transaction ID: ${broadcast}`)
-
-      return broadcast
-    } catch (err) {
-      console.log(`Error in sendAllBCH()`)
       throw err
     }
   }
@@ -181,7 +113,7 @@ class CoinJoin extends Command {
     //const server = flags.server
     //if (!server || server === "")
     //  throw new Error(`You must specify the CoinJoin server.`)
-    flags.server = "http://localhost:5000/"
+    flags.server = "http://localhost:5000"
 
     return true
   }
