37a4b058fcc649ad2fd2f0bfcbef83f9c9a29d3f
Chris Troutner <chris.troutner@gmail.com>
2018-11-27 18:05:58 -0800

    Adding REST API call to get server info

--- src/commands/find-coinjoin.js
+++ src/commands/find-coinjoin.js
@@ -5,25 +5,16 @@
 
 "use strict"
 
-const IPFS = require("ipfs")
-const OrbitDB = require("orbit-db")
+const rp = require("request-promise")
+
+const Network = require("ccoinjoin-network")
+const network = new Network()
 
 const util = require("util")
 util.inspect.defaultOptions = {
   showHidden: true,
   colors: true,
   depth: 1
-}
-
-// OrbitDB uses Pubsub which is an experimental feature
-// and need to be turned on manually.
-// Note that these options need to be passed to IPFS in
-// all examples in this document even if not specified so.
-const ipfsOptions = {
-  start: true,
-  EXPERIMENTAL: {
-    pubsub: true
-  }
 }
 
 const { Command, flags } = require("@oclif/command")
@@ -35,90 +26,45 @@ class FindCoinJoin extends Command {
     this.findServers()
   }
 
-  findServers() {
+  async findServers() {
     const globalThis = this
 
     try {
-      // Create an IPFS instance.
-      const ipfs = new IPFS(ipfsOptions)
+      const options = {
+        method: "GET",
+        uri: `http://coinjoin.christroutner.com:5000/ipfsid`,
+        resolveWithFullResponse: true,
+        json: true
+      }
 
-      ipfs.on("error", err => console.error(err))
+      const result = await rp(options)
+      //console.log(`ipfs ID: ${util.inspect(result.body.ipfsid)}`)
+      const ipfsid = result.body.ipfsid
 
-      ipfs.bootstrap.add(
-        "/ip4/162.243.158.213/tcp/4002/ipfs/QmQLcCWKuxsMkRt5DJkcSw361iNLikjEBMEj6ovR6pwAb5"
-      )
+      const centralServer = `/dns4/coinjoin.christroutner.com/tcp/4002/ipfs/${ipfsid}`
+      network.bootstrap.push(centralServer)
 
-      // Once IPFS is ready, initialize the DB.
-      ipfs.on("ready", async () => {
-        try {
-          console.log(`IPFS is ready.`)
+      console.log(`network.bootstrap: ${util.inspect(network.bootstrap)}`)
 
-          const access = {
-            // Give write access to everyone
-            write: ["*"]
-          }
+      // Connect to the IPFS network and subscribe to the DB.
+      await network.connectToIPFS()
 
-          // Create OrbitDB instance
-          const orbitdb = new OrbitDB(ipfs)
+      const res = await network.ipfs.swarm.connect(centralServer)
+      console.log(`res: ${util.inspect(res)}`)
 
-          const db = await orbitdb.eventlog("ccoinjoin", access)
-          await db.load() // Load any saved state from disk.
+      const latest = await network.readDB()
+      console.log(`latest: ${util.inspect(latest)}`)
 
-          console.log(`IPFS address: ${db.address}`)
+      setInterval(async function() {
+        const peers = await network.ipfs.swarm.peers()
+        console.log(`peers: ${peers.length}`)
 
-          // List bootstrap peers, to verify it's been updated with the bootstrap list.
-          //const list = await ipfs.bootstrap.list()
-          //console.log(`list: ${util.inspect(list)}`)
-
-          console.log(`Curent list of servers:`)
-          await getLatest(db)
-
-          // Debugging: Display a notification whenever the DB is updated by another
-          // node.
-          console.log(`Waiting 30 seconds for P2P DB to send updates...`)
-          db.events.on("replicated", () => {
-            console.log(`...new entries found. Updating server list.`)
-
-            // Get the latest servers after the DB has replicated.
-            getLatest(db)
-          })
-
-          setTimeout(function() {
-            console.log(`Exiting.`)
-            //globalThis.exit(1)
-            process.exit()
-          }, 1000 * 30)
-        } catch (err) {
-          console.error(`Error trying to initialize OrbitDB: `, err)
-          process.exit()
-        }
-      })
+        const latest = await network.readDB()
+        console.log(`latest: ${util.inspect(latest)}`)
+      }, 1000 * 30)
     } catch (err) {
       console.log(`Error in findServers(): `, err)
     }
-  }
-}
-
-async function getLatest(db) {
-  try {
-    // latest[4] is the latest entry
-    const latest = db.iterator({ limit: 5 }).collect()
-
-    //console.log(`latest: ${JSON.stringify(latest, null, 2)}`)
-    /*
-    console.log(` `)
-    console.log(`latest 3 servers:`)
-    console.log(`${util.inspect(latest[0].payload.value)}`)
-    console.log(`${util.inspect(latest[1].payload.value)}`)
-    console.log(`${util.inspect(latest[2].payload.value)}`)
-*/
-    const servers = []
-    for (var i = 0; i < latest.length; i++)
-      servers.push(latest[i].payload.value)
-
-    console.log(`servers: ${JSON.stringify(servers, null, 2)}`)
-  } catch (err) {
-    console.log(`Error in getLatest(): `, err)
   }
 }
 
